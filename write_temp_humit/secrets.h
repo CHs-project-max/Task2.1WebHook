9ead8636b945e1c4120e7579e2518262b5532ec2
CHs-project-max <laychhayhout010@gmail.com>
2025-03-14 15:46:05 +1100

    Add files via upload

---
+++ write_temp_humit/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "我是美女"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "kimkim480"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2877129		// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "T5YTZRJFHQS3ZH63"   // replace XYZ with your channel write API Key
